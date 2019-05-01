1b6051d82378954610c3f40bf129bef6f98179bf
Tobias Eskou <5831278@RTS-365.dk>
2019-05-01 11:46:34 +0200

    categories

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,24 +1,62 @@
-document.addEventListener('DOMContentLoaded', function(){
-    let dropdown = document.querySelectorAll('.nav_submenu');
-    let nav_category = document.querySelectorAll('.navigation__category')
-    
-    dropdown.forEach((e,index) => {
-        console.log(e, index)
-    });
-    nav_category[1].style.transition = 'all 1s';
-    dropdown.addEventListener('mouseover', function(){
-        nav_category[1].style.opacity = '1';
-        nav_category[1].style.top = 'calc(100% + 1px)';
+document.addEventListener('DOMContentLoaded', function () {
+    let dropdown = document.querySelector('.nav_submenu');
+    let nav_category = document.querySelector('.navigation__category');
 
-        dropdown.addEventListener('mouseout', function(){
-            /* nav_category[1].style.transition = 'all 1s'; */
-            nav_category[1].style.opacity = '0';
-            nav_category[1].style.top = 'calc(100% + 20px)';
+    nav_category.style.transition = 'all 0.3s';
+    dropdown.addEventListener('mouseover', function () {
+        nav_category.style.opacity = '1';
+        nav_category.style.top = 'calc(100% + 1px)';
+        nav_category.style.visibility = 'visible';
+
+        dropdown.addEventListener('mouseout', function () {
+            fadeOut(nav_category)
         })
     })
 
+    let dropdown2 = document.querySelector('.nav_submenu2');
+    let nav_category2 = document.querySelector('.navigation__category2');
+
+    nav_category2.style.transition = 'all 0.3s';
+    dropdown2.addEventListener('mouseover', function () {
+        nav_category2.style.opacity = '1';
+        nav_category2.style.top = 'calc(100% + 1px)';
+        nav_category2.style.visibility = 'visible';
+
+        dropdown2.addEventListener('mouseout', function () {
+            fadeOut(nav_category2)
+        })
+    })
+
+    let header = document.querySelector('#header');
+    let nav = document.querySelector('#nav')
+    let nav__logo = document.querySelector('.nav__logo')
+
+    document.onscroll = (function () {
+
+        if (document.documentElement.scrollTop > 50) {
+            nav.classList.add("nav-scrolled");
+            header.classList.add("nav-scrolled");
+            nav__logo.classList.add("nav__logo-scrolled");
+        } else {
+            nav.classList.remove("nav-scrolled");
+            header.classList.remove("nav-scrolled");
+            nav__logo.classList.remove("nav__logo-scrolled");
+        }
+
+    });
 
 });
-/* dropdown.addEventListener('mouseout' function(event){
-nav_category[1].removeAttribute('style');
-}) */
+
+function toggle_visibility(id) {
+    var e = document.getElementById(id);
+    if (e.style.bottom == '-80px')
+        e.style.bottom = '0';
+    else
+        e.style.bottom = '-80px';
+ }
+
+function fadeOut(nc) {
+    nc.style.opacity = '0';
+    nc.style.top = 'calc(100% + 20px)';
+    nc.style.visibility = 'hidden';
+}
