bd4fba9249a2f1bff4e70dd39e1840b6518a7ae3
Tobias Eskou <5831278@RTS-365.dk>
2019-05-02 10:22:43 +0200

    categories sort-box

---
+++ scripts/categories.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let dropdown3 = document.querySelector('.sorter');
+    let nav_category3 = document.querySelector('.sorting__list');
+
+    nav_category3.style.transition = 'all 0.3s';
+    dropdown3.addEventListener('mouseover', function () {
+        nav_category3.style.opacity = '1';
+        nav_category3.style.top = 'calc(100% + -10px)';
+        nav_category3.style.visibility = 'visible';
+
+        dropdown3.addEventListener('mouseout', function () {
+            fadeOut3(nav_category3)
+        })
+    })
+});
+
+function fadeOut3(nc) {
+    nc.style.opacity = '0';
+    nc.style.top = 'calc(100% + 20px)';
+    nc.style.visibility = 'hidden';
+}
