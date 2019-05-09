95c2413453cb396dc5e1c0203a6bee25656fa400
Tobias Eskou <5831278@RTS-365.dk>
2019-05-09 12:05:47 +0200

    product

--- scripts/categories.js
+++ scripts/categories.js
@@ -15,10 +15,95 @@ document.addEventListener('DOMContentLoaded', function () {
         })
     })
 
-    // Showing 12 results
-    let amount = 12 ;
+    // Products
+    let products = [
+        { img: "pictures/product_1.jpg", desc: "Smart Phone", price: 670, tag: "product__new" },
+        { img: "pictures/product_2.jpg", desc: "Smart Phone", price: 520, tag: "product__sale" },
+        { img: "pictures/product_3.jpg", desc: "Smart Phone", price: 710, tag: "none" },
+        { img: "pictures/product_4.jpg", desc: "Smart Phone", price: 330, tag: "none" },
+        { img: "pictures/product_5.jpg", desc: "Smart Phone", price: 170, tag: "none" },
+        { img: "pictures/product_6.jpg", desc: "Smart Phone", price: 240, tag: "product__hot" },
+        { img: "pictures/product_7.jpg", desc: "Smart Phone", price: 70, tag: "none" },
+        { img: "pictures/product_8.jpg", desc: "Smart Phone", price: 490, tag: "product__hot-red" },
+        { img: "pictures/product_9.jpg", desc: "Smart Phone", price: 410, tag: "product__hot-red" },
+        { img: "pictures/product_10.jpg", desc: "Smart Phone", price: 365, tag: "product__hot-red" },
+        { img: "pictures/product_11.jpg", desc: "Smart Phone", price: 195, tag: "product__hot-red" },
+        { img: "pictures/product_12.jpg", desc: "Smart Phone", price: 580, tag: "product__hot-red" }
+    ]
+    
+    //Sorting box
+    document.querySelector('.sorter').addEventListener('click', function (event) {
+        if (event.target.localName == "li") {
+            document.querySelector('.option').textContent = event.target.textContent;
+        }
 
-    document.querySelector(".counter").textContent = `Showing ${amount} results` ;
+    })
+
+    document.querySelector('.default').addEventListener('click', function () {
+        sortDefault()
+        renderProducts()
+    })
+    
+    document.querySelector('.price').addEventListener('click', function () {
+        sortPrice()
+        renderProducts()
+    })
+    
+    document.querySelector('.desc').addEventListener('click', function () {
+        sortDesc()
+        renderProducts()
+    })
+    
+    
+    // Sorting function - Create index as key in list
+    products.map((product, idx) => {
+        product.index = idx
+        return product
+    })
+    
+    function sortDefault() {
+        products.sort((a, b) => a.index - b.index)
+    }
+    
+    function sortPrice() {
+        products.sort((a, b) => a.price - b.price)
+    }
+    
+    function sortDesc() {
+        products.sort((a, b) => {
+            if (a.desc < b.desc) {
+                return -1
+            } else if (a.desc > b.desc) {
+                return 1
+            } else {
+                return 0
+            }
+        })
+    }
+    
+    renderProducts()
+    function renderProducts() {
+        document.querySelector('.product__container').innerHTML = ""
+        products.forEach(Element => {
+            document.querySelector('.product__container').innerHTML +=
+            `<div class="product">
+            <div class="product_img"><img src="${Element.img}" alt="">
+            <a href="categories.html" class="${Element.tag}"></a>
+            </div>
+            <div class="product__content">
+            <a href="#" class="product__text">${Element.desc}</a>
+            <div class="product__price">$${Element.price}</div>
+            </div>
+            </div>
+            `
+        })
+    }
+
+
+    // Showing results
+    let amount = document.querySelectorAll('.product__container .product').length;
+
+    document.querySelector(".counter").textContent = `Showing ${amount} results`;
 
 });
 
